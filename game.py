f64a6b4f2deb95b211656e9d1dfbbc8037bfd869
Iani Gayo <i_jirahmae@me.com>
2023-10-27 15:38:08 +0100

    TODO pseudocode for validation biopsy game

---
+++ game.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import SimpleITK as sitk
+import pandas as pd 
+import PIL
+from matplotlib import pyplot 
+
+if __name__ == '__main__':
+    
+    # TODO: Load up environments (patient datasets)
+    
+    
+    # TODO : Load up agents / suggested actions 
+    
+    
+    # TODO : Initialise user interface 
+    
+    
+    # TODO : Interact through actions (user or RL suggested actions)
+    
+    
+    
